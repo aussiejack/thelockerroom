c2ae227c4a19915255a2d1b1ad043aaf7d7712c9
aussiejack <31354636+aussiejack@users.noreply.github.com>
2017-08-25 13:13:37 -0500

    oops

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname));
+//app.use(express.static(__dirname));
 
 // views is directory for all template files
 //app.set('views', __dirname + '/views');
 //app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.sendFile('index.htm')
+  response.sendFile('/index.htm')
 });
 
 app.listen(app.get('port'), function() {
